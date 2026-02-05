375d2278859ce69a3aa09277e793e5bb844cfe98
PetrusGeorge <petrus.george.leal@gmail.com>
2026-02-05 14:04:08 -0300

    Parallel run benchmark

--- manager_bm.py
+++ manager_bm.py
@@ -1,12 +1,13 @@
 import os
-import time
+import argparse
 
-bm_dir = '../mlp_tudao_naive'
-inst_list_file = 'mailist-cp'
+bm_dir = "../mlp_testao_naive"
+inst_list_file = 'listinha'
 inst_list = []
+min_test = 10
 
-min_test = 1
-
+#sources = ["java", "dotnet", "python3", "pypy", "julia", "cpp", "cpp-OOP",
+         #"fortran", "node", "luajit", "rust", "c", "golang"]
 sources = ["java", "dotnet", "python3", "pypy", "julia", "cpp", "cpp-OOP",
          "fortran", "node", "lua", "luajit", "rust", "c", "swift", "golang"]
 
@@ -18,7 +19,7 @@ lang_dir = {
         "pypy": "python",
         "julia": "julia",
         "cpp" : "cplusplus",
-        "cppOOP" : "cppOOP",
+        "cpp-OOP" : "cppOOP",
         "c" : "c",
         "fortran" : "fortran",
         "node" : "javascript",
@@ -31,7 +32,7 @@ lang_dir = {
         }
 
 
-if os.path.isdir(bm_dir) == False:
+if not os.path.isdir(bm_dir):
     os.mkdir(bm_dir)
 
 with open(inst_list_file) as f:
@@ -40,31 +41,18 @@ with open(inst_list_file) as f:
 
 def count(source, lang, inst, path):
 
+    print(bm_dir)
     f_name = os.path.join(bm_dir, lang+'.csv')
     c = 0
-    if os.path.isfile(f_name) == False:
+    if not os.path.isfile(f_name):
         return c
     with open(f_name) as f:
         for line in f:
-            if line.find(source) > 0 and line.find(inst) > 0:
+            if line.find(source) >= 0 and line.find(inst) >= 0:
                 c += 1
 
     return c
 
-<<<<<<< HEAD
-for i in range(min_test):
-    for s in sources:
-        for inst in inst_list:
-
-            crnt_time = time.localtime()
-            hour = crnt_time.tm_hour
-            day = crnt_time.tm_wday
-
-            #while (hour >= 9 and hour < 21):# and day < 5:
-                #time.sleep(5)
-                #hour = time.localtime().tm_hour
-
-=======
 
 parser = argparse.ArgumentParser(description='Rodador Tudao')
 parser.add_argument('--lang' , nargs='+', required=True, help='Sources: python3, java, mcs, dotnet, julia, cpp, lua, javascript, swift, golang')
@@ -89,9 +77,8 @@ print(inst_list)
 for i in range(min_test):
     for s in sources:
         for inst in inst_list:
->>>>>>> 481d9ef (Some fixes to the benchmark scripts)
             if count(s, lang_dir[s], inst, bm_dir) < min_test:
-                os.system(f'python3 run_bm.py -i {inst} --lang {s}')
+                os.system(f'python3 run_bm.py -i {inst} --lang {s} -n {min_test} --par true')
 
 
 
